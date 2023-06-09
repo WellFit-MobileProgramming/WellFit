a98df5f27dc07e63f587934a0256ec56c57bf60d
chaeyoung103 <thdcodud103@naver.com>
2023-06-09 18:21:15 +0900

    feat: 파이어베이스 로그인 회원가입 구현

--- app/src/main/java/com/example/wellfit/SignupActivity2.kt
+++ app/src/main/java/com/example/wellfit/SignupActivity2.kt
@@ -3,10 +3,14 @@ package com.example.wellfit
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
 import com.example.wellfit.databinding.ActivitySignup2Binding
+import java.util.regex.Pattern
 
 class SignupActivity2 : AppCompatActivity() {
     lateinit var binding: ActivitySignup2Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignup2Binding.inflate(layoutInflater)
@@ -14,10 +18,37 @@ class SignupActivity2 : AppCompatActivity() {
         initLayout()
     }
     private fun initLayout() {
+        val intent = Intent(this,SignupActivity3::class.java)
+        val name = intent.getStringExtra("name")
+        val birth = intent.getStringExtra("birth")
+        val phone = intent.getStringExtra("phone")
+        val gender = intent.getStringExtra("gender")
+
         val moveBtn = binding.moveSignup
         moveBtn.setOnClickListener{
-            val intent = Intent(this,SignupActivity3::class.java)
-            startActivity(intent)
+            val email = binding.idEt.text.toString()
+            val pw = binding.pwEt.text.toString()
+            val pwCertification = binding.pwCertificationEt.text.toString()
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                Toast.makeText(this, "이메일 형식이 아닙니다", Toast.LENGTH_SHORT).show()
+            }else{
+                if (!Pattern.matches("^(?=.*\\d)(?=.*[~`!@#$%^&*()-])(?=.*[a-zA-Z]).{8,20}$", pw)) {
+                    Toast.makeText(this, "비밀번호 형식을 지켜주세요.", Toast.LENGTH_SHORT).show()
+                }else{
+                    if(!pw.equals(pwCertification)){
+                        Toast.makeText(this, pw.equals(pwCertification).toString(), Toast.LENGTH_SHORT).show()
+//                        Toast.makeText(this, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show()
+                    }else{
+                        intent.putExtra("name",name)
+                        intent.putExtra("birth",birth)
+                        intent.putExtra("phone",phone)
+                        intent.putExtra("gender",gender)
+                        intent.putExtra("email",email)
+                        intent.putExtra("pw",pw)
+                        startActivity(intent)
+                    }
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/wellfit/SignupActivity3.kt
+++ app/src/main/java/com/example/wellfit/SignupActivity3.kt
@@ -3,22 +3,47 @@ package com.example.wellfit
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.wellfit.databinding.ActivitySignup3Binding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SignupActivity3 : AppCompatActivity() {
     lateinit var binding: ActivitySignup3Binding
+    private var auth : FirebaseAuth? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignup3Binding.inflate(layoutInflater)
+        auth = Firebase.auth
         setContentView(binding.root)
         initLayout()
     }
 
     private fun initLayout() {
+        val name = intent.getStringExtra("name")
+        val birth = intent.getStringExtra("birth")
+        val phone = intent.getStringExtra("phone")
+        val gender = intent.getStringExtra("gender")
+        val email = intent.getStringExtra("email")
+        val pw = intent.getStringExtra("pw")
         val moveBtn = binding.moveHome
         moveBtn.setOnClickListener{
-            val intent = Intent(this,MainActivity::class.java)
+            if (email != null && pw !=null) {
+                createAccount(email,pw, intent)
+            }
+            val intent = Intent(this,LoginActivity::class.java)
             startActivity(intent)
         }
+    }
+    private fun createAccount(email:String,pw:String,intent: Intent){
+        auth?.createUserWithEmailAndPassword(email, pw)
+            ?.addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    intent.putExtra("signup",true)
+                } else {
+                    intent.putExtra("signup",false)
+                }
+            }
     }
 }
